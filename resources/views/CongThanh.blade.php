cb6cd2c5c6c4f68f76b780c1a873d3fb197a06db
Công Thành <you@example.com>
2018-06-23 12:22:24 +0700

    change

--- resources/views/CongThanh.blade.php
+++ resources/views/CongThanh.blade.php
@@ -1,6 +1,6 @@
 <form action="{{route('inten')}}" method="post">
 	@csrf     <!-- BAO MAT POST  -->  
-	<input type="text" name="ten" placeholder="nhap ten" >
-	<input type="submit">
+	<input type="text" name="ten" placeholder="nhap ten" id="ten">
+	<input type="submit"  >
 
 </form>
